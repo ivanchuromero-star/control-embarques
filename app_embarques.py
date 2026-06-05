2cc61c6b5322730689a6eae3ecf22e380341384b
ivanchuromero-star <ivanchuromero@gmail.com>
2026-06-05 14:42:25 -0600

    Update app_embarques.py

--- app_embarques.py
+++ app_embarques.py
@@ -75,7 +75,7 @@ def init_db():
     conn.close()
 
 
-def seed_from_excel(file_path):
+def seed_from_excel('OPERACIONES COMERCIO.xlsx'):
     if not Path(file_path).exists():
         return
     conn = get_conn()
